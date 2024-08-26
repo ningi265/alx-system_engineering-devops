58aac7e696c0a17eec2b03152b47513ff217163c
ningi265 <ningintonga@gmail.com>
2024-08-25 23:00:48 -0700

    fix2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ return number of subscribers of a subredit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
+    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
     headers = {'User-Agent': 'my-app/0.0.1'}
 
     try:
