5041f28df65ad7a86ecd8cf01ec7a875a60a44a8
legendmw <brianmtonga592@gmail.com>
2024-08-28 17:47:13 +0200

    fix

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -9,10 +9,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        result = number_of_subscribers(sys.argv[1])
-        
-        # Check if the result is an integer and format accordingly
-        if isinstance(result, int):
-            print("{:d}".format(result))
-        else:
-            print(result)
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,9 @@ Script that queries the number of subscribers on a given Reddit subreddit.
 """
 
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
-    """Return 'OK' if the subreddit exists but fails to fetch subscribers, otherwise return 0."""
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
 
@@ -20,24 +19,10 @@ def number_of_subscribers(subreddit):
             data = response.json()
             subscribers = data.get('data', {}).get('subscribers', 0)
             return subscribers
-
-        # If the subreddit exists but some other error occurs, return "OK"
-        return "OK"
+        
+        # If the status code is not 200, return 0
+        return 0
 
     except requests.RequestException:
         # Return 0 if any exception occurs
         return 0
-
-# Check if script is being run with a subreddit argument
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        subreddit = sys.argv[1]
-        result = number_of_subscribers(subreddit)
-        
-        # Correctly format the output based on its type
-        if isinstance(result, int):
-            print("{:d}".format(result))
-        else:
-            print(result)
-    else:
-        print("Please provide a subreddit as an argument.")
