7e2f379b0d72a7feb2b1cba859575ae574e5a5e3
legendmw <brianmtonga592@gmail.com>
2024-08-28 12:47:41 +0200

    z

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """
-0-main
+Script that queries the number of subscribers on a given Reddit subreddit.
 """
-import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    try:
+        # Perform the GET request without following redirects
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the status code is 200 OK
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        
+        # If status code is not 200, return 0
+        return 0
+    
+    except requests.RequestException:
+        # Return 0 if any exception occurs
+        return 0
