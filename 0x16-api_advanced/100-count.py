043b7e8f0265c0a095cbd97cd3c033d56d45dd7b
legendmw <brianmtonga592@gmail.com>
2024-08-29 16:27:23 +0200

    ff

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
 This module defines a function that recursively queries the Reddit API,
-parses the titles of all hot articles, and prints a sorted count of given keywords.
+parses the titles of all hot articles, 
+and prints a sorted count of given keywords.
 """
 
 import requests
 import re
 from collections import defaultdict
 
-def count_words(subreddit, word_list, hot_list=[], after=None, counts=None):
+
+def count_words(
+        subreddit, word_list, hot_list=[], after=None, counts=None
+        ):
     """Recursively counts the occurrences of keywords in hot posts for a given subreddit."""
     if counts is None:
         counts = defaultdict(int)
